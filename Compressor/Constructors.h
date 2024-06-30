15edf09a14f81ed73e33cf8fc3ed0ea3371f8bc9
Henry Boyer <HenryBoyerCodes@gmail.com>
2024-06-30 17:10:17 -0500

    removed compressor

--- Compressor/Constructors.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "pch.h"
-
-#include "Skeleton.h"
-
-Skeleton ConstructSkeleton(const aiScene* scene);
--- Compressor/Unpack.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-
-#include "Definitions.h"
-
-#include "Compressor.h"
-
-void Compressor::UnpackFile() {
-
-}
